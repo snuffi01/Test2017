2e7fbdc2f78e8d6ef07ce2bbde0ce2b17fb50c05
Kristian Karlsson <kristian.karlsson@kamoda.se>
2017-02-02 08:33:11 +0100

    Änddringar

--- Swift Test2017/ViewController.swift
+++ Swift Test2017/ViewController.swift
@@ -4,15 +4,27 @@
 //
 //  Created by Kristian Karlsson on 2017-02-01.
 //  Copyright © 2017 Kamoda. All rights reserved.
-//
+//  Nisses tuggummi
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var UILabel_top: UILabel!
+    @IBAction func ButtonTapped(_ sender: Any) {
+        //Laget if
+        // nisses tuggummifabrik
+        
+        UILabel_top.text = "IF Elfsborg"
+    }
+    @IBAction func Knapp2ButtonTapped(_ sender: Any) {
+        UILabel_top.text = "Buttons are cool!"
+        print("Usertapped Button")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
